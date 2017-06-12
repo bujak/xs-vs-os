7b296fd766f7a50865b9d6dd987c37bfa5448d6f
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-12 16:04:28 +0200

    Add Range enum

---
+++ src/main/java/com/codecool/xvso/model/Range.java
@@ -0,0 +1,14 @@
+package com.codecool.xvso.model;
+
+/**
+ * com.codecool.xvso.model
+ * Created by bujak on 12.06.17.
+ */
+public enum  Range {
+
+    MINIMAL(1),
+    MAXIMAL(3);
+
+    Range(int i) {
+    }
+}
