5256f893459237daee03d3eb6dbe846a27f7a0fd
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-12 15:55:18 +0200

    Add Cell class

--- src/main/java/com/codecool/xvso/model/Cell.java
+++ src/main/java/com/codecool/xvso/model/Cell.java
@@ -4,4 +4,15 @@ package com.codecool.xvso.model;
  * Created by pgurdek on 12.06.17.
  */
 public class Cell {
+
+    private Seed content;
+    private Integer row;
+    private Integer col;
+
+    public Cell(Integer row, Integer col) {
+        this.row = row;
+        this.col = col;
+    }
+
+
 }
