1ef9380ac46021ffb16df67d2a82ac8c7eae2d5a
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-13 09:22:15 +0200

    Add BoardTest

---
+++ src/test/java/com/codecool/xvso/model/BoardTest.java
@@ -0,0 +1,16 @@
+package com.codecool.xvso.model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * com.codecool.xvso.model
+ * Created by bujak on 13.06.17.
+ */
+class BoardTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+}
