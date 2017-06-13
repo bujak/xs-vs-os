4c889b1e787e6a4846b24e3d5c93ac0d0c34b2ab
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-13 10:34:12 +0200

    Add Board methods bodies

--- src/main/java/com/codecool/xvso/model/Board.java
+++ src/main/java/com/codecool/xvso/model/Board.java
@@ -4,4 +4,29 @@ package com.codecool.xvso.model;
  * Created by pgurdek on 12.06.17.
  */
 public class Board {
+    Cell[][] cells;
+
+    public Cell[][] getCells() {
+        return this.cells;
+    }
+
+    public Integer getMoveCounter() {
+        return null;
+    }
+
+    public void setMoveCounter(int i) {
+    }
+
+    public void init() {
+    }
+
+    public Boolean hasWon(Seed nought, int i, int i1) {
+        return null;
+    }
+
+    public void updateCell(Seed nought, int i, int i1) {
+    }
+
+    public boolean isDraw() {
+    }
 }
