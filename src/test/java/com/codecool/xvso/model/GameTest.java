b523730121a304b2c2915b14c0b461ae5ffb0d75
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-14 09:20:08 +0200

    Add GameTest

---
+++ src/test/java/com/codecool/xvso/model/GameTest.java
@@ -0,0 +1,14 @@
+package com.codecool.xvso.model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * com.codecool.xvso.model
+ * Created by bujak on 14.06.17.
+ */
+class GameTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+}
