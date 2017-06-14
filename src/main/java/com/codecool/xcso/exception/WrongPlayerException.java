242048b23d51622f35b98994276428acf33ed624
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-14 10:33:57 +0200

    Add WrongPlayerException

---
+++ src/main/java/com/codecool/xcso/exception/WrongPlayerException.java
@@ -0,0 +1,8 @@
+package com.codecool.xcso.exception;
+
+/**
+ * com.codecool.xcso.exception
+ * Created by bujak on 14.06.17.
+ */
+public class WrongPlayerException extends IllegalArgumentException {
+}
