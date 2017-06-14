13ef8d272da2246459f7f40c00c508a989249b88
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-14 14:07:16 +0200

    Add SeedTest

---
+++ src/test/java/com/codecool/xvso/app/model/SeedTest.java
@@ -0,0 +1,18 @@
+package com.codecool.xvso.app.model;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * com.codecool.xvso.app.model
+ * Created by bujak on 14.06.17.
+ */
+class SeedTest {
+    @ParameterizedTest
+    @CsvSource(value = {"CROSS, X", "NOUGHT, O", "EMPTY, ' '"})
+    void seedGetValueReturnProperValue(Seed seed, String value) {
+        assertEquals(seed.getValue(), value);
+    }
+}
