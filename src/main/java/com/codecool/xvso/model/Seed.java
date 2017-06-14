b6e6f4183a231bc897b24fb62aedfde3df5184e5
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-14 11:35:20 +0200

    Add Seed enum values

--- src/main/java/com/codecool/xvso/model/Seed.java
+++ src/main/java/com/codecool/xvso/model/Seed.java
@@ -4,7 +4,22 @@ package com.codecool.xvso.model;
  * Created by pgurdek on 12.06.17.
  */
 public enum Seed {
-    EMPTY,
-    CROSS,
-    NOUGHT
+    EMPTY(" "),
+    CROSS("X"),
+    NOUGHT("O");
+
+    String seedValue;
+
+    Seed(String value) {
+        this.seedValue = value;
+    }
+
+    public String getValue() {
+        return this.seedValue;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
