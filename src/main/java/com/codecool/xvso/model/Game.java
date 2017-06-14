d71a3ab58c213b560200bd5d91ded9015b323bd4
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-14 09:39:54 +0200

    Add declaration of methods in Game class

--- src/main/java/com/codecool/xvso/model/Game.java
+++ src/main/java/com/codecool/xvso/model/Game.java
@@ -4,4 +4,27 @@ package com.codecool.xvso.model;
  * Created by pgurdek on 12.06.17.
  */
 public class Game {
+    Board board;
+    GameState currentState;
+    Seed currentPlayer;
+
+    public Game() {
+
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void initGame(){
+
+    }
+
+    public void updateGameState(Seed seed, int row, int column){
+
+    }
+
+    public Seed choseRandomFirstSeed() {
+        return currentPlayer;
+    }
 }
