a334b68caf36e7102f2a13024c38afa47c5a07cd
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-14 10:06:21 +0200

    Add Tests for Game class

--- src/test/java/com/codecool/xvso/model/GameTest.java
+++ src/test/java/com/codecool/xvso/model/GameTest.java
@@ -1,9 +1,12 @@
 package com.codecool.xvso.model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * com.codecool.xvso.model
@@ -26,6 +29,26 @@ class GameTest {
         assertSame(board, game.getBoard());
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"CROSS,1,1", "NOUGHT, 1,2", "CROSS,2,3", "NOUGHT,3,3"})
+    void isUpdateGameStateUpdateUpdateProperCells(Seed seed, int row, int column){
+        game.initGame();
+        game.updateGameState(seed, row, column);
+        Cell cell = new Cell(row,column);
+        cell.setContent(seed);
+        assertSame(cell, game.getBoard().getCells()[row][column]);
+    }
 
+    @RepeatedTest(10)
+    void isCurrentPlayerChosenAfterGameInit() {
+        game.initGame();
+        assertTrue(game.getCurrentPlayer().equals(Seed.CROSS) || game.getCurrentPlayer().equals(Seed.NOUGHT));
+    }
 
+    @RepeatedTest(10)
+    void isChoseRandomFirstPlayerDoNotSetSeedEmpty() {
+        game.initGame();
+        assertFalse(game.getCurrentPlayer().equals(Seed.EMPTY));
+
+    }
 }
