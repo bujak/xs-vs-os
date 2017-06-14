62082bfba0380819206fe2768a8ffddd727318c3
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-14 09:40:16 +0200

    Add isInitGameInitializedEmptyBoard in GameTest

--- src/test/java/com/codecool/xvso/model/GameTest.java
+++ src/test/java/com/codecool/xvso/model/GameTest.java
@@ -1,14 +1,31 @@
 package com.codecool.xvso.model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 /**
  * com.codecool.xvso.model
  * Created by bujak on 14.06.17.
  */
 class GameTest {
+
+    Game game;
+
     @BeforeEach
     void setUp() {
+        game = new Game();
     }
+
+    @Test
+    void isInitGameInitializedEmptyBoard() {
+        game.initGame();
+        Board board = new Board();
+        board.init();
+        assertSame(board, game.getBoard());
+    }
+
+
 
 }
