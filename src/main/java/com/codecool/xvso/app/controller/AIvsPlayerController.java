b476bf7a7bc8ed8a1ba5c15713d5dbfcd27aa07b
Tomek Bujakowski <bujakowski@gmail.com>
2017-06-16 00:54:38 +0200

    Add working AI

--- src/main/java/com/codecool/xvso/app/controller/AIvsPlayerController.java
+++ src/main/java/com/codecool/xvso/app/controller/AIvsPlayerController.java
@@ -15,28 +15,43 @@ public class AIvsPlayerController {
     private Game game;
     private UserInterface userInterface = new UserInterface();
     private Seed currentPlayer;
+    private Seed humanPlayer;
 
     public AIvsPlayerController(Game game) {
         this.game = game;
-        this.ai = new AI(game);
+
+
+    }
+
+    public void setCurrentPlayer(Seed currentPlayer) {
+        this.currentPlayer = currentPlayer;
     }
 
     public void startGame() {
         game.initGame();
+        this.ai = new AI(game, game.getCurrentPlayer());
+        humanPlayer = game.getCurrentPlayer();
         do {
-            currentPlayer = game.getCurrentPlayer();
             userInterface.showGameBoard(game.getBoard().getCells());
-            userInterface.showPlayerTurn(currentPlayer);
+            userInterface.showPlayerTurn(humanPlayer);
             Integer row = userInterface.getIntegerInput("Enter row: ");
             Integer column = userInterface.getIntegerInput("Enter Column: ");
-            game.updateGameState(currentPlayer, row, column);
-            ai.makeMove(currentPlayer);
+            game.updateGameState(humanPlayer, row, column);
+            this.setCurrentPlayer(humanPlayer);
+            if (game.getCurrentState().equals(GameState.PLAYING)) {
+                ai.makeMove();
+                this.setCurrentPlayer(ai.getSeed());
+            }
         } while (game.getCurrentState().equals(GameState.PLAYING));
 
         if (game.getCurrentState().equals(GameState.DRAW)) {
+            userInterface.showGameBoard(game.getBoard().getCells());
+
             userInterface.draw();
         } else {
+            userInterface.showGameBoard(game.getBoard().getCells());
             userInterface.showWinner(currentPlayer);
+
         }
     }
 }
--- src/main/java/com/codecool/xvso/app/model/ai/AI.java
+++ src/main/java/com/codecool/xvso/app/model/ai/AI.java
@@ -3,7 +3,6 @@ package com.codecool.xvso.app.model.ai;
 import com.codecool.xvso.app.model.CellRange;
 import com.codecool.xvso.app.model.Game;
 import com.codecool.xvso.app.model.Seed;
-import sun.nio.cs.ext.SimpleEUCEncoder;
 
 import java.util.Random;
 
@@ -13,33 +12,44 @@ import java.util.Random;
  */
 public class AI {
 
-    Game game;
-    Seed seed;
+    private Game game;
+    private Seed seed;
 
-    public AI(Game game) {
+    public AI(Game game, Seed seed) {
         this.game = game;
-        seed = Seed.NOUGHT;
+        setAIPlayer(seed);
+    }
+
+    public Seed getSeed() {
+        return seed;
+    }
+
+    private void setAIPlayer(Seed seed) {
+        if (seed.equals(Seed.CROSS)) {
+            this.seed = Seed.NOUGHT;
+        } else {
+            this.seed = Seed.CROSS;
+        }
+
     }
 
     private int randomCoordGenerator() {
         Random random = new Random();
         return random.nextInt(CellRange.MAXIMAL.getValue() - CellRange.MINIMAL.getValue() + 1) + CellRange.MINIMAL.getValue();
-
     }
 
-    public void makeMove(Seed seed) {
+    public void makeMove() {
         int randomRow = randomCoordGenerator();
         int randomColumn = randomCoordGenerator();
         boolean notMoved = true;
         while (notMoved) {
-            if (game.getBoard().getCells()[randomRow-1][randomColumn-1].getContent().equals(Seed.EMPTY)) {
-                game.updateGameState(seed, randomRow, randomColumn);
+            if (game.getBoard().getCells()[randomRow - CellRange.ARRAYMODIFIER.getValue()][randomColumn - CellRange.ARRAYMODIFIER.getValue()].getContent().equals(Seed.EMPTY)) {
+                game.updateGameState(this.seed, randomRow, randomColumn);
                 notMoved = false;
             }
             randomRow = randomCoordGenerator();
             randomColumn = randomCoordGenerator();
         }
     }
-
 }
 
